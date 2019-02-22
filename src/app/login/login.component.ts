c925d95ec5c646da89baf4a0c0517f5223be0ac4
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2019-02-22 16:28:40 +0530

    updated register and sos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { baseURL } from '../shared/baseurl';
 import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
+declare var google: any;
 
 export interface register{
   name: string;
@@ -14,7 +15,9 @@ export interface register{
   phone:string;
   address:string,
   city:string,
-  state:string
+  state:string,
+  latitude:number,
+  longitude:number
 }
 @Component({
   selector: 'app-login',
@@ -25,8 +28,15 @@ export class LoginComponent implements OnInit {
   login=true;
   rFormGroup: FormGroup;
   reg: register;
+  show=true;
+  isChecked = false;
   user = {username: '', password: '', remember: false};
   errMess: string;
+  latitude:number;
+  longitude:number;
+  geocoder;
+  city:string;
+  state:string;
   constructor(private fb: FormBuilder,public dialogRef: MatDialogRef<LoginComponent>,
     private authService: AuthService, public http:HttpClient, @Inject(SESSION_STORAGE) private storage: StorageService) { }
 
@@ -47,7 +57,8 @@ export class LoginComponent implements OnInit {
       email: ['', [Validators.required, Validators.email ]],
       address:['',[Validators.required,,Validators.minLength(7)]],
       city:['',[Validators.required,Validators.minLength(3)]],
-      state:['',[Validators.required,Validators.minLength(3)]]
+      state:['',[Validators.required,Validators.minLength(3)]],
+      location:[false]
     });
   }
 
@@ -85,8 +96,75 @@ export class LoginComponent implements OnInit {
         this.errMess = error
       });
   }  
-  
+  onChkChange(){
+      if(this.isChecked){
+         this.show=false;
+         if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position)=>{
+            this.latitude=position.coords.latitude; 
+            this.longitude=position.coords.longitude; 
+            /*this.displayLocation(position.coords.latitude,position.coords.longitude);*/
+            this.geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        
+            this.geocoder.geocode(
+                {'latLng': latlng}, 
+                (results, status) =>{
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            var add= results[0].formatted_address ;
+                            var  value=add.split(", ");
+        
+                            var count=value.length;
+                            var country=value[count-1];
+                            this.state=value[count-2];
+                            this.city=value[count-3];
+                            console.log("city name is: " +this.city+" country: "+country+" state: "+this.state);
+                            console.log("add= "+add);
+                            this.rFormGroup.patchValue({'address':add,'city':this.city,'state':this.state})
+                        }
+                        else  {
+                            console.log("address not found");
+                        }
+                    }
+                    else {
+                        console.log("Geocoder failed due to: " + status);
+                    }
+                }
+            );
+           });
+           
+          }
+      }else{
+        this.show=true;
+      }
+
+  }
+
   onSubmitRegister(){
+    if(!this.isChecked){
+      this.geocoder = new google.maps.Geocoder();
+      let address=this.rFormGroup.get('address').value+' ,'+this.rFormGroup.get('city').value+', '+this.rFormGroup.get('state').value;
+      this.geocoder.geocode( { 'address': address},(results, status)=> {
+        if (status == google.maps.GeocoderStatus.OK) {
+          this.latitude = results[0].geometry.location.lat();
+          this.longitude = results[0].geometry.location.lng();
+          alert(this.latitude);
+          this.reg= {
+            name:this.rFormGroup.get("name").value,
+            password:this.rFormGroup.get("password").value,
+            username:this.rFormGroup.get("username").value,
+            phone:this.rFormGroup.get("phone").value,
+            email:this.rFormGroup.get("email").value,
+            address:this.rFormGroup.get("address").value,
+            city:this.rFormGroup.get("city").value,
+            state:this.rFormGroup.get("state").value,
+            latitude:this.latitude,
+            longitude:this.longitude 
+         }
+        } 
+      }); 
+    }else{
     this.reg= {
       name:this.rFormGroup.get("name").value,
       password:this.rFormGroup.get("password").value,
@@ -95,7 +173,10 @@ export class LoginComponent implements OnInit {
       email:this.rFormGroup.get("email").value,
       address:this.rFormGroup.get("address").value,
       city:this.rFormGroup.get("city").value,
-      state:this.rFormGroup.get("state").value 
+      state:this.rFormGroup.get("state").value,
+      latitude:this.latitude,
+      longitude:this.longitude 
+    }
    }
    console.log(this.reg);
    this.authService.signUp(this.reg)
