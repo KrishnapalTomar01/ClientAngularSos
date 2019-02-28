2e29ad7189169d75ca1a75beca6a0b7a528a3e91
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2019-02-28 23:37:41 +0530

    Expandable rows

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -9,12 +9,22 @@ import {StatisticsService} from '../services/statistics.service';
 export class StatisticsComponent implements OnInit {
   displayedColumns: string[] = ['State', 'City', 'Total Victims', 'Victims Rescued'];
   statsTotal:any[];
+  months:string[]=['Jan','Feb','March','April','May','June','July','Aug','Sept','Oct','Nov','Dec'];
+  selectedElement:  any = undefined;
   errMess:string;
   constructor(private statisticsservice:StatisticsService) { }
 
   ngOnInit() {
       this.statisticsservice.getTotalCounts().subscribe(stats=>this.statsTotal=stats,
         err=>this.errMess=<any>err);
+  }
+
+  showTable(child): void {
+    if (this.selectedElement && this.selectedElement._id.city == child._id.city && this.selectedElement._id.state==child._id.state) {
+      this.selectedElement = undefined;
+    } else {
+      this.selectedElement = child;
+    }
   }
 
 }
